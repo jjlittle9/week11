4ec9c66f796f6e88e5f62ba572fe758203879db0
jjlittle9 <65914772+jjlittle9@users.noreply.github.com>
2020-11-12 17:40:09 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,4 +1,27 @@
 
+$('form').submit(function(event) {
+
+  event.preventDefault();
+
+  var form_champion_data = $(this).serializeArray();
+
+  var champion_object = condense_data(form_champion_data);
+
+  save_champion_to_local_storage(champion_object);
+
+  display_champions_in_table();
+});
+
+function condense_data(form_champion_data) {
+  var object = {};
+
+  $(form_champion_data).each(function() {
+    object[this.name] = this.value;
+  });
+
+  return object;
+}
+
 function save_champion_to_local_storage() {
   var champions_array = JSON.parse(localStorage.getItem("champions"));
 
@@ -10,4 +33,17 @@ function save_champion_to_local_storage() {
   champion_array.push(champion_object);
 
   localStorage.setItem('champions', JSON.stringify(champion_array));
+}
+
+// Updating a text box right below the ranges to show what number is selected
+function updateTextInput(val) {
+  document.getElementById('textInput').value=val; 
+}
+
+function display_champions_in_table() {
+  var champions = JSON.parse(localStorage.getItem('champions'));
+
+  $(champions).each(function() {
+    $("#champions").append(`<p>${this.champion_name} - ${this.games_won} - ${this.games_lost}</p>`);
+  });
 }
