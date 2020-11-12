d67e44baede2b2491bc25b39072d0eda7bb0c728
jjlittle9 <65914772+jjlittle9@users.noreply.github.com>
2020-11-12 13:55:02 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,1 +1,13 @@
-// Put stuff here
+
+function save_champion_to_local_storage() {
+  var champions_array = JSON.parse(localStorage.getItem("champions"));
+
+  // Making sure that champions_array exists and if it doesn't making it an empty array
+  if(!champions_array) {
+    champions_array = [];
+  }
+
+  champion_array.push(champion_object);
+
+  localStorage.setItem('champions', JSON.stringify(champion_array));
+}
